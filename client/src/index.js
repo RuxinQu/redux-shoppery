33470730148db158add2a4a0c1a96e1ce56a7b56
Ruxin Qu <rosheenqu@gmail.com>
2023-01-23 16:46:58 -0800

    refactor product slice using redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import store from "./utils/GlobalState";
+
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // to pass the store object to all child components
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
